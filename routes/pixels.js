3b7810aaf43f441ba2bd7a3161d7bc9446d1aecb
joselages <jose.lages.d12@gmail.com>
2022-01-18 22:57:32 +0000

    added update id pixel is already in DB

--- routes/pixels.js
+++ routes/pixels.js
@@ -17,14 +17,27 @@ router.post('/', async (req, res) => {
 
     const newPixel = req.body;
 
-
     try{
-        const pixel = await new Pixel(newPixel);
+
+        const exists = await Pixel.find({x:newPixel.x, y:newPixel.y}).lean();
         
-        await pixel.save();
+        let pixel;
+
+        if(exists.length){
+
+            pixel = await Pixel.findByIdAndUpdate( exists[0]._id, newPixel,{
+                new: true,
+              });
+
+        } else {  
+            pixel = await new Pixel(newPixel);
+            
+            await pixel.save();
+        }
         
-        res.status(202).send(newPixel);
+        res.status(202).send(pixel);
     } catch(ex){
+        console.log(ex)
         res.status(400).send({ message: "Bad request" });
     }
 
